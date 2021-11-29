437e39d2ab74a00300988a60ba744bf7f32d7b34
aniljogireddy <anil.jogireddy@gmail.com>
2021-11-29 17:03:53 +0100

    lab done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import logo from './logo.svg';
+import ironhack from './images/ironhack-logo-xs.png';
+import menu from './images/menu-top-xs.png';
+import icon1 from './images/icon1.png';
+import icon2 from './images/icon2.png';
+import icon3 from './images/icon3.png';
+import icon4 from './images/icon4.png'
+
+function SayHello(){
+  return(
+    
+
+    <div>
+      <img class="logo" src={ironhack} alt="IronHackLogo"/>
+      <img class="menu" src={menu} alt="Menu"/>
+      <h1>Say hello to ReactJS</h1>
+      <p>You will learm how to use</p>
+      <p>the most popular forntend library,</p>
+      <p>and become a super Ninja developer.</p>
+      <div className="button">
+        <button>Awesome!</button></div>
+       <div class="images">
+       <img src={icon1} alt="icon1" title="Declaration" description="React makes it painless to create interactive Uis."/>
+        <img src={icon2} alt="icon2"/>
+        <img src={icon3} alt="icon3"/>
+        <img src={icon4} alt="icon4"/>
+         </div> 
+    </div>
+    
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SayHello />
     </div>
   );
 }
+
+
+
 
 export default App;
